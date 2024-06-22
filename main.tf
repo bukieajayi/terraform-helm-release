84585b08067c0f4d7c0f4827f97eefc66b377c66
bukieajayi <bukieajayi23@gmail.com>
2024-06-22 01:57:51 -0500

    updated readme

--- main.tf
+++ main.tf
@@ -15,39 +15,3 @@ image:
   EOF
   ]
 }
-
-module "python" {
-  source    = "./module"
-  namespace = "default"
-  name      = "python"
-  wait      = false
-  chart     = "./application"
-  values = [<<EOF
-  replicaCount: 3
-
-image:
-  repository: python
-  pullPolicy: IfNotPresent
-  # Overrides the image tag whose default is the chart appVersion.
-  tag: "latest"
-  EOF
-  ]
-}
-
-module "nodejs" {
-  source    = "./module"
-  namespace = "default"
-  name      = "nodejs"
-  wait      = false
-  chart     = "./application"
-  values = [<<EOF
-  replicaCount: 3
-
-image:
-  repository: nodejs
-  pullPolicy: IfNotPresent
-  # Overrides the image tag whose default is the chart appVersion.
-  tag: "latest"
-  EOF
-  ]
-}
