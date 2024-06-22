70c805adc1118dcc8d5c72656ee67cb413cefb7d
bukieajayi <bukieajayi23@gmail.com>
2024-06-22 00:58:44 -0500

    updated variables

--- module/main.tf
+++ module/main.tf
@@ -1,18 +1,7 @@
-resource "helm_release" "wordpress" {
-  name  = "wordpress"
-  chart = "../application"
-  wait  = false 
-
-  values = [<<EOF
-
-  replicaCount: 3
-
-image:
-  repository: nginx
-  pullPolicy: IfNotPresent
-  # Overrides the image tag whose default is the chart appVersion.
-  tag: "latest"
-
-  EOF
-  ]
+resource "helm_release" "app" {
+  name  = var.name
+  chart = var.chart
+  wait  = var.wait
+  values = var.values
 }
+
