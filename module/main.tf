c03342bc0a2166636d1194df57669a0a636d78f5
bukieajayi <bukieajayi23@gmail.com>
2024-06-22 00:32:05 -0500

    updated code

--- module/main.tf
+++ module/main.tf
@@ -1,5 +1,18 @@
 resource "helm_release" "wordpress" {
   name  = "wordpress"
   chart = "../application"
-  wait  = false
+  wait  = false 
+
+  values = [<<EOF
+
+  replicaCount: 3
+
+image:
+  repository: nginx
+  pullPolicy: IfNotPresent
+  # Overrides the image tag whose default is the chart appVersion.
+  tag: "latest"
+
+  EOF
+  ]
 }
